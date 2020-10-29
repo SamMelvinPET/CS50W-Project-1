4a777415ee6c99bc0572314dec1cd312edcc6bba
Sam <sampcmelvin@gmail.com>
2020-10-29 21:36:34 +0000

    wiki-TITLE functionality added

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
+from markdown2 import markdown
 
 from . import util
 
@@ -7,4 +10,17 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def viewpage(request, name):
+    pagedata = util.get_entry(name)
+
+    if pagedata == None:
+        return render(request, "encyclopedia/entrynotfound.html")
+    
+    return render(request, "encyclopedia/entry.html", {
+        "pagedata": markdown(pagedata),
+        "pagetitle": name
+        })
+
+
 
